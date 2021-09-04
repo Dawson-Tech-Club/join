33f9f1fda7f4e98e9aca54e7c675cca15721f64e
STR1Z <weihengjiang64@hotmail.com>
2021-09-04 19:06:01 -0400

    website thumbnail

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+// @ts-ignore image exists
+import iconImage from "../images/icon.png";
+
 export function Head({ name }: { name: string }) {
 	return (
-		<Helmet>
-			<title>Join DTC - {name}</title>
-		</Helmet>
+		<Helmet
+			title={"Join DTC - " + name}
+			meta={[
+				{
+					property: `og:title`,
+					content: "Join the Dawson Tech Club now!",
+				},
+				{
+					property: `og:type`,
+					content: `website`,
+				},
+				{
+					property: `og:image`,
+					content: `https://join.dawsontech.club/${iconImage}`,
+				},
+			]}
+		/>
 	);
 }
